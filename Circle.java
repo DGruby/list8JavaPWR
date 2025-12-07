cfb56a705607dfd681a13d37142162206056d33e
Damian Grzywacz <damian.grzywacz17@gmail.com>
2025-12-07 11:14:23 +0100

    added fields, constructors and getters for sphere and circle

--- Circle.java
+++ Circle.java
@@ -1,2 +1,20 @@
 public class Circle {
+
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
 }
--- Sphere.java
+++ Sphere.java
@@ -1,2 +1,25 @@
 public class Sphere {
+
+    private final double radius;
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getSurfaceArea() {
+        return 4 * Math.PI * Math.pow(radius, 2);
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double getVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }
 }
+
